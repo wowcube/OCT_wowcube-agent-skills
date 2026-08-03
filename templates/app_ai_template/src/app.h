f0b86ebfc6552de74b7d6128d9f7d83f698ec70c
ihortrosin <igor.trosin92@gmail.com>
2026-08-03 18:43:43 +0300

    feat(template): full beta define set in app.h (TITLE, GUID1, CATEGORIES, COLORS)

--- templates/app_ai_template/src/app.h
+++ templates/app_ai_template/src/app.h
@@ -1,4 +1,10 @@
 #pragma once
 #include "app_ai_template.h"
 #define APP_VERSION 100 //v1.00
+#define APP_TITLE "app_ai_template"
 #define APP_DIR "..\\..\\app_ai_template"
+#define APP_GUID1 0x0000000000000000ULL
+//APP_CATEGORY_GAME     - plain app/game (default)
+//APP_CATEGORY_LAUNCHER - autoruns as the cube's HOME screen (see octavios/app_template/src/app.h)
+#define APP_CATEGORIES (APP_CATEGORY_GAME)
+#define APP_COLORS 0x00000000
